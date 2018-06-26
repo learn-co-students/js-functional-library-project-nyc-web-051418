8c80f76ff9e994ac4a025e6d4350811c09761070
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-26 14:12:40 -0400

    finished map and each

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      let newCollect = (collection instanceof(Array)) ? collection.slice() : Object.values(collection)
 
+      for (let i = 0; i < newCollect.length; i++) {
+        iteratee(newCollect[i]) }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, iteratee) {
+      let newCollect = (collection instanceof(Array)) ? collection.slice() : Object.values(collection)
+      newArr = [];
+      for (let i = 0; i < newCollect.length; i++){
+        newArr.push(iteratee(newCollect[i]))
+      }
+      return newArr;
     },
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
